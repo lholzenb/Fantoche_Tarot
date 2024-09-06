64e7ea1afdf162ad14c64966ce751db19265b84a
Manuela Widmer <aleunam97@outlook.de>
2024-09-06 22:43:51 +0200

    Added AudioManager for Background Sound

---
+++ Fantoche_Tarot_Unity/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+        public static AudioManager instance = null;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+        }
+    }
