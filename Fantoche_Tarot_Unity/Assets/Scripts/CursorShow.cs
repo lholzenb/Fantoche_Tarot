cf35e5e5357ebb4afd2dff3d555f903fc92b020c
Manuela Widmer <aleunam97@outlook.de>
2024-09-08 14:02:44 +0200

    Cursor on ScreenRecorder

---
+++ Fantoche_Tarot_Unity/Assets/Scripts/CursorShow.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEditor;
+
+public class CursorShow : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.SetCursor(PlayerSettings.defaultCursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
